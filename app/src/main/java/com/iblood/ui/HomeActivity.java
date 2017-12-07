4cbc107a9491c40591283b1a6d7d205df1f0d662
asus <308827964@qq.com>
2017-12-07 21:23:27 +0800

    王海强

--- app/src/main/java/com/iblood/ui/HomeActivity.java
+++ app/src/main/java/com/iblood/ui/HomeActivity.java
@@ -1,18 +1,23 @@
 package com.iblood.ui;
 
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
@@ -21,13 +26,10 @@ import android.widget.Toast;
 import com.iblood.R;
 import com.iblood.app.ActivityCollector;
 import com.iblood.app.App;
-import com.iblood.app.FragmentManger;
 import com.iblood.base.BaseActivity;
 import com.iblood.base.BaseFragment;
-import com.iblood.contract.OnlinePresenter;
-import com.iblood.ui.manage.ManageFragment;
-import com.iblood.ui.online.OnlineFragment;
-import com.iblood.ui.personal.PersonalFragment;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -52,16 +54,17 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
     RelativeLayout relHome;
     @BindView(R.id.mBottomGroup)
     RadioGroup mBottomGroup;
-    @BindView(R.id.mFrame)
-    FrameLayout mFrame;
     @BindView(R.id.nav_view)
     NavigationView navView;
     @BindView(R.id.activity_home)
     DrawerLayout activityHome;
     @BindView(R.id.cehua_shenqing)
     Button cehuaShenqing;
+    @BindView(R.id.list_home)
+    ListView listHome;
     private FragmentManager fragmentManager;
     private long mExitTime;
+    private View v;
 
     @Override
     protected int getLayoutId() {
@@ -96,12 +99,9 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
         cehuaShenqing.setOnClickListener(this);
     }
 
-    //添加初始Fragment
     @Override
     protected void initData() {
-        fragmentManager = App.mBaseActivity.getSupportFragmentManager();
-        OnlineFragment onlineFragment = (OnlineFragment) FragmentManger.getInstance().start(R.id.mFrame, OnlineFragment.class, false).build();
-        new OnlinePresenter(onlineFragment);
+
     }
 
     @Override
@@ -114,13 +114,46 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.mOLBtn:
-                FragmentManger.getInstance().start(R.id.mFrame, new OnlineFragment().getClass(), false).build();
+                //显示popuwindow
+                v = LayoutInflater.from(HomeActivity.this).inflate(R.layout.fragment_online, null);
+                //创建一个popuwindow对象
+                PopupWindow popu = new PopupWindow(v, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                //默认获取不到焦点，设置获取焦点
+                popu.setFocusable(true);
+                //点击窗口以外区域，窗口消失
+                popu.setBackgroundDrawable(new BitmapDrawable());
+                //弹出或者消失的时候带动画效果
+//                popu.setAnimationStyle(R.style.mypopu);
+                //显示popuwindow
+                popu.showAsDropDown(mBottomGroup, 0, 0);
                 break;
             case R.id.mMangerBtn:
-                FragmentManger.getInstance().start(R.id.mFrame, new ManageFragment().getClass(), false).build();
+                //显示popuwindow
+                v = LayoutInflater.from(HomeActivity.this).inflate(R.layout.fragment_manage, null);
+                //创建一个popuwindow对象
+                PopupWindow popu1 = new PopupWindow(v, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                //默认获取不到焦点，设置获取焦点
+                popu1.setFocusable(true);
+                //点击窗口以外区域，窗口消失
+                popu1.setBackgroundDrawable(new BitmapDrawable());
+                //弹出或者消失的时候带动画效果
+//                popu.setAnimationStyle(R.style.mypopu);
+                //显示popuwindow
+                popu1.showAsDropDown(mBottomGroup, 0, 0);
                 break;
             case R.id.mPersonalBtn:
-                FragmentManger.getInstance().start(R.id.mFrame, new PersonalFragment().getClass(), false).build();
+                //显示popuwindow
+                v = LayoutInflater.from(HomeActivity.this).inflate(R.layout.fragment_personal, null);
+                //创建一个popuwindow对象
+                PopupWindow popu2 = new PopupWindow(v, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                //默认获取不到焦点，设置获取焦点
+                popu2.setFocusable(true);
+                //点击窗口以外区域，窗口消失
+                popu2.setBackgroundDrawable(new BitmapDrawable());
+                //弹出或者消失的时候带动画效果
+//                popu.setAnimationStyle(R.style.mypopu);
+                //显示popuwindow
+                popu2.showAsDropDown(mBottomGroup, 0, 0);
                 break;
         }
     }
