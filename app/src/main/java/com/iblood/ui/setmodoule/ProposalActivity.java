d66f8e3c0e638247e6099a37756c2e0eab239b60
QingChun <327943014@qq.com>
2017-12-07 09:36:30 +0800

    青春    2017 12 07 上传

---
+++ app/src/main/java/com/iblood/ui/setmodoule/ProposalActivity.java
@@ -0,0 +1,48 @@
+package com.iblood.ui.setmodoule;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.iblood.R;
+import com.iblood.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ProposalActivity extends BaseActivity {
+
+    @BindView(R.id.proposal_edittext)
+    EditText proposalEdittext;
+    @BindView(R.id.proposal_textview)
+    TextView proposalTextview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_proposal;
+    }
+
+    @Override
+    protected void initView() {
+
+        setContentView(R.layout.activity_proposal);
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+}
