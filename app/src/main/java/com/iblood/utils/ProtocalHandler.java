6a6c5d13b4e81c90b44cbaf777822775e9b6f6bf
特雷西麦克格蕾蒂 <327943014@qq.com>
2017-12-13 11:29:04 +0800

    创建LIBs 目录

---
+++ app/src/main/java/com/iblood/utils/ProtocalHandler.java
@@ -0,0 +1,166 @@
+package com.iblood.utils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.UnknownHostException;
+
+public abstract class ProtocalHandler {
+	private static final int CONN_TIMEOUT = 10 * 1000;
+	private static final int READ_TIMEOUT = 15 * 1000;
+	protected String recommandURL = "";
+
+	protected byte[] getProtoBufferRequest() {
+		String userdata = getUserJSONString();
+		String unencoder = getRequestString(userdata);
+		return unencoder.getBytes();
+	}
+
+	protected String getRequestString(String userdata) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("loctype=1");
+		sb.append("&tableid=").append(AppUtils.TABLE_ID);
+		sb.append("&key=").append(AppUtils.KEY);
+		if (userdata.indexOf("{") == -1) {
+			sb.append("&ids=").append(userdata);
+		} else {
+			sb.append("&data=").append(userdata);
+		}
+		return sb.toString();
+	}
+
+	protected abstract String getUserJSONString();
+
+	protected String getUrl(String str) {
+		String url = "";
+		if (str.equals("create")) {
+			url = AppUtils.SAVE_URL;
+		} else if (str.equals("update")) {
+			url = AppUtils.UPDATE_URL;
+		} else if (str.equals("delete")) {
+			url = AppUtils.DELETE_URL;
+		}
+		return url;
+	}
+
+	public String getData(String str) {
+		String result = null;
+		result = getDataMayThrow(str);
+		return result;
+	}
+
+	private String getDataMayThrow(String str) {
+		HttpURLConnection conn = null;
+		InputStream input = null;
+		String result = null;
+		recommandURL = getUrl(str);
+		byte[] entityBytes = getProtoBufferRequest();
+		conn = makePostRequest(recommandURL, entityBytes);
+		try {
+			input = conn.getInputStream();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		result = parseResponse(input);
+		if (input != null) {
+			try {
+				input.close();
+			} catch (IOException e) {
+			}
+			input = null;
+		}
+		if (conn != null) {
+			conn.disconnect();
+			conn = null;
+
+		}
+		return result;
+	}
+
+	protected String parseResponse(InputStream inputStream) {
+		try {
+			return new String(inputStreamToByte(inputStream));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	protected HttpURLConnection makePostRequest(String path, byte[] entityBytes) {
+		if (path == null) {
+			return null;
+		}
+		HttpURLConnection conn = null;
+		try {
+			URL url = new URL(path);
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setInstanceFollowRedirects(true);
+			conn.setConnectTimeout(CONN_TIMEOUT);
+			conn.setReadTimeout(READ_TIMEOUT);
+			conn.setDoInput(true);
+			conn.setDoOutput(true);
+			conn.setUseCaches(false);
+			conn.setRequestProperty("Content-Type",
+					"application/x-www-form-urlencoded");
+			conn.setRequestProperty("Content-Length",
+					String.valueOf(entityBytes.length));
+			conn.setRequestProperty("Connection", "Keep-Alive");
+			conn.connect();
+			OutputStream outStream = conn.getOutputStream();
+			outStream.write(entityBytes);
+			outStream.flush();
+			outStream.close();
+			if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+			}
+		} catch (UnknownHostException e) {
+		} catch (MalformedURLException e) {
+		} catch (SocketTimeoutException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+		}
+		return conn;
+	}
+
+	protected byte[] readStream(InputStream inStream) {
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		try {
+			byte[] buffer = new byte[1024];
+			int length = -1;
+			while ((length = (inStream.read(buffer))) != -1) {
+				outStream.write(buffer, 0, length);
+				outStream.flush();
+			}
+		} catch (IOException e) {
+		} finally {
+			try {
+				outStream.close();
+			} catch (IOException e) {
+			}
+			if (inStream != null) {
+				try {
+					inStream.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+		return outStream.toByteArray();
+	}
+
+	public byte[] inputStreamToByte(InputStream in) throws IOException {
+		int BUFFER_SIZE = 2048;
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		byte[] data = new byte[BUFFER_SIZE];
+		int count = -1;
+		while ((count = in.read(data, 0, BUFFER_SIZE)) != -1)
+			outStream.write(data, 0, count);
+
+		data = null;
+		return outStream.toByteArray();
+	}
+}
---
+++ app/src/main/java/com/iblood/utils/ToastUtil.java
@@ -0,0 +1,91 @@
+/**
+ * @描述		:	
+ * @作�??	    :Android - csx
+ * @创建日期  :2016�?3�?12�? 上午11:18:12  
+ *
+ */
+package com.iblood.utils;
+
+import android.content.Context;
+import android.content.res.Resources.NotFoundException;
+import android.os.Handler;
+import android.widget.Toast;
+
+/**
+ * @描述 :Toast工具�?
+ * @作�?? :Android - csx
+ * @创建日期 :2016�?3�?12�? 上午11:18:12
+ * 
+ */
+public class ToastUtil {
+
+	// 全局的上下文
+	private static Context context = null;
+	private static Toast mToast;
+	private static Handler mHandler = new Handler();
+	private static Runnable r = new Runnable() {
+		public void run() {
+			mToast.cancel();
+		}
+	};
+
+	public static void makeText(Context mContext, String text) {
+		try {
+			mHandler.removeCallbacks(r);
+			if (mToast != null)
+				mToast.setText(text);
+			else
+				mToast = Toast.makeText(mContext, text, Toast.LENGTH_LONG);
+			mHandler.postDelayed(r, 1000);
+
+			mToast.show();
+		} catch (Exception e) {
+		}
+	}
+
+	public static void makeText(Context mContext, int resId) {
+		try {
+			makeText(mContext, mContext.getResources().getString(resId));
+		} catch (NotFoundException e) {
+		}
+	}
+
+	/**
+	 * 
+	 * @描述 :初始化方�?
+	 * @方法名称 :init----&gt;ToastUtil.java
+	 * @作�?? :Android - csx
+	 * @创建日期 :2016�?3�?12�? 上午11:18:54
+	 * 
+	 */
+	public static void init(Context mContext) {
+		context = mContext;
+	}
+
+	/**
+	 * 
+	 * @描述 :文字提示
+	 * @方法名称 :show----&gt;ToastUtil.java
+	 * @作�?? :Android - csx
+	 * @创建日期 :2016�?3�?12�? 上午11:25:12
+	 * @param txt
+	 * 
+	 */
+	public static void show(String txt) {
+		makeText(context, txt);
+	}
+
+	/**
+	 * 
+	 * @描述 :id提示
+	 * @方法名称 :show----&gt;ToastUtil.java
+	 * @作�?? :Android - csx
+	 * @创建日期 :2016�?3�?12�? 上午11:25:47
+	 * @param resId
+	 * 
+	 */
+	public static void show(int resId) {
+		makeText(context, resId);
+	}
+
+}
