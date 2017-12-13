5207f45d54f3f9a400f8d0a4404657deb6ba2941
特雷西麦克格蕾蒂 <327943014@qq.com>
2017-12-13 11:35:23 +0800

    创建LIBs 目录  外加注册

--- app/src/main/java/com/iblood/ui/loginactivity/ReginActivity.java
+++ app/src/main/java/com/iblood/ui/loginactivity/ReginActivity.java
@@ -1,12 +1,33 @@
 package com.iblood.ui.loginactivity;
 
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.iblood.R;
 import com.iblood.base.BaseActivity;
+import com.iblood.config.Urls;
+import com.iblood.ui.menu.MainActivity;
+import com.iblood.utils.AppUtils;
+import com.iblood.utils.CJSON;
+import com.iblood.utils.ConnectionUtils;
+import com.iblood.utils.Md5Encrypt;
+import com.iblood.utils.SignUtil;
+import com.iblood.utils.TokenUtil;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class ReginActivity extends BaseActivity {
     private TextView reg_fnishi;
@@ -43,7 +64,55 @@ public class ReginActivity extends BaseActivity {
       regin_qq =  (ImageView) findViewById(R.id.regin_qq);
       regin_wx =  (ImageView) findViewById(R.id.regin_wx);
     }
+    public void gethttpdata() {
+        OkHttpClient okHttpClient=new OkHttpClient();
+        FormBody.Builder builder = new FormBody.Builder();
+        TokenUtil.init(ReginActivity.this);
+        String token = TokenUtil.createToken();
+        Log.e("to",token);
+        Request.Builder request= new Request.Builder();
+        String ip = ConnectionUtils.getIp(this);
+        Map<String,Object> map=new HashMap<>();
+        String pass="17647572011qc";
+        String s = Md5Encrypt.md5(pass, "UTF-8");
+        map.put("userPhone","17647572011");
+        map.put("userName","qingchunyeye");
+        map.put("password",s);
+        AppUtils.setAppContext(ReginActivity.this);
+        String s1 = CJSON.toJSONMap(map);
+        Log.e("DA",s1);
+        builder.add("data",s1);
 
+//       String s1 = CJSON.toJSONMap(map);
+//       Log.e("DD",o.toString());
+        for (String key: map.keySet()){
+            Object value = map.get(key);
+            builder.add(key, (String) value);
+            Log.e("TAG",value+"---------");
+        }
+        String linkString = SignUtil.createLinkString(map);
+        request.addHeader("sign",linkString);
+        request.addHeader("ip",ip);
+        request.addHeader("token",token);
+        request.addHeader("channel","android");
+
+        Request build1 = request.url(Urls.HOT).post(builder.build()).build();
+        okHttpClient.newCall(build1).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                String message = e.getMessage();
+                Toast.makeText(ReginActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String data = response.body().string();
+                Log.e( "onResponse: ",data );
+
+            }
+        });
+
+    }
     @Override
          protected void initData() {
 
